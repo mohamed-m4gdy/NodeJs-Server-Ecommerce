b6a1b37f6db3eaede92f63fb4190115095a993ac
mohamed-m4gdymohamed-m4gdy <evilonlyone@gmail.com>
2023-08-18 00:22:08 -0700

    init

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  rules: {
+    // semi: ['error', 'never'],
+    quotes: ['error', 'single'],
+    'prettier/prettier': 2, // means error
+    'no-console': 1, // means Warning
+    'no-var': 'error',
+    'prefer-const': 'error',
+  },
+};
